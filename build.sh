0242679313a503de5b443c5407ff87823db107c3
dmitriy.tseyler <dmitriy.tseyler@jetbrains.com>
2018-07-07 18:42:37 +0300

    pass arguments with parameters

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ cd driver && gradle clean model:jar jar && cd ..
 mkdir driver-pack
 cp driver/build/libs/driver.jar driver-pack/driver.jar
 cd driver-pack
-echo "java -jar driver.jar" > run.sh
+echo "java -jar driver.jar \$1 \$2 \$3" > run.sh
 chmod +x run.sh
