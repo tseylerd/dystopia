cf365c21fb53199ab25f9dd8ea84a5b65bd6ab29
dmitriy.tseyler <dmitriy.tseyler@jetbrains.com>
2018-07-07 18:18:25 +0300

    build run script

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
+rm -r driver-pack
 cd driver && gradle clean model:jar jar && cd ..
+mkdir driver-pack
+cp driver/build/libs/driver.jar driver-pack/driver.jar
+cd driver-pack
+echo "java jar driver.jar" > run.sh
+chmod +x run.sh
