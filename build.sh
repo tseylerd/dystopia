057e5c00a611e17b6ac8fe22214b830f01569df8
dmitriy.tseyler <dmitriy.tseyler@jetbrains.com>
2018-07-10 19:10:24 +0300

    configuration

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
-rm -r driver-pack
+rm -r pack
 cd driver && gradle clean model:jar jar && cd ..
-mkdir driver-pack
-cp driver/build/libs/driver.jar driver-pack/driver.jar
-cd driver-pack
-echo "java -jar driver.jar \$1 \$2 \$3" > run.sh
-chmod +x run.sh
+cd client && gradle clean jar && cd ..
+
+mkdir pack
+cp driver/build/libs/driver.jar pack/driver.jar
+cp client/build/libs/client.jar pack/client.jar
+cd pack
+echo 'java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 "$(dirname "$0")"/driver.jar $1 $2 $3' >> run-driver.sh
+chmod +x run-driver.sh
+echo "echo 'running client'" > run-client.sh
+echo "java -jar client.jar \$1 \$2 \$3" >> run-client.sh
+chmod +x run-client.sh
